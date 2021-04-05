1f620f927cc5c1d3aa017ecf1a215b72013a326d
Vanzoon <celedshka@gmail.com>
2021-04-06 01:02:18 +0300

    post list view changed

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,7 +1,9 @@
-from django.views.generic import View
+from django.http import Http404
+from django.views.generic import View, ListView, DetailView, CreateView
 from django.db.models import Q
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic.detail import BaseDetailView
 
 from .forms import TagForm, PostForm
 from .pagination import pagination
@@ -11,22 +13,33 @@ from .utils import *
 # TODO: optionally rewrite self-made mixins - they are obscure for query optimization
 
 
-def posts_list(request):
-    search_query = request.GET.get('search', '')
-    if search_query:
-        posts = Post.objects.filter(
-            Q(title__icontains=search_query) | Q(body__icontains=search_query)) \
-            .select_related('author') \
-            .prefetch_related('tags')
-    else:
-        posts = Post.objects.all() \
-            .select_related('author') \
-            .prefetch_related('tags')
+class PostListView(ListView):
+    model = Post
+    paginate_by = 4
+    template_name = 'blog/index.html'
 
-    context = pagination(
-        posts, requested_page=request.GET.get('page', 1)
-    )
-    return render(request, 'blog/index.html', context=context)
+    def get_queryset(self):
+        search_query = self.request.GET.get('search')
+        if search_query:
+            print(self.request.GET.get('search'))
+
+            queryset = self.model.objects.filter(
+                Q(title__icontains=search_query) |
+                Q(body__icontains=search_query)) \
+                .select_related('author') \
+                .prefetch_related('tags')
+        else:
+            queryset = self.model.objects.all() \
+                .select_related('author') \
+                .prefetch_related('tags')
+
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -34,11 +47,19 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class PostCreate(LoginRequiredMixin, PermissionRequiredMixin, ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     form_model = PostForm
     template = 'blog/post_create_form.html'
     raise_exception = True
     permission_required = 'blog.add_post'
+
+    def post(self, request, slug):
+        form = self.form_model(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+
+
+
 
 
 class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
@@ -57,9 +78,22 @@ class PostDelete(LoginRequiredMixin, ObjectDetailMixin, View):
     permission_required = 'blog.delete_post'
 
 
-class TagDetail(ObjectDetailMixin, View):
+class TagDetail(BaseDetailView):
     model = Tag
-    template = 'blog/tag_detail.html'
+    template_name = 'blog/tag_detail.html'
+
+    def get(self, request, slug):
+        self.object = None
+        try:
+            self.object = self.model.objects.get(slug__iexact=slug)
+        except self.model.DoesNotExist:
+            raise Http404(f'No {self.model.__name__} matches the given query.')
+
+        context = {
+            self.model.__name__.lower(): self.object,
+            'admin_obj': self.object,
+        }
+        return render(request, self.template_name, context=context)
 
 
 class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
